a87b68b189d60bdfd08cf0284a58db83353a4f1f
Nehal Wandhare <nehalwandhare000@gmail.com>
2026-03-25 17:09:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,12 @@ resource "aws_vpc" "main" {
     Name = "my-vpc"
   }
 }
+
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "my-subnet"
+  }
+}
