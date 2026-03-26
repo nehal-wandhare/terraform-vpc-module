54451d5cafa596b8c031d928528f28cf095177e7
Nehal Wandhare <nehalwandhare000@gmail.com>
2026-03-26 19:43:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,19 @@
-variable "cidr_block" {}
-variable "subnet_cidr" {}
-variable "vpc_name" {}
+variable "cidr_block" {
+  description = "VPC CIDR"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "Subnet CIDR"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Base VPC name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment (dev/test/prod)"
+  type        = string
+}
